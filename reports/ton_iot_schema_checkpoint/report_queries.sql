5d00b2fd1a831576c8dfe36aaa56ab6607c13712
Matheus Antony Souza Pereira <matheus.antony@ufv.br>
2026-09-07 00:33:06 -0300

    Delete reports directory

--- reports/ton_iot_schema_checkpoint/report_queries.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM "report_issues" ORDER BY "column" ASC;
-SELECT * FROM "report_affected_partitions" ORDER BY "partition" ASC;
-SELECT * FROM "report_checks" ORDER BY "check" ASC;
-SELECT * FROM "report_files" ORDER BY "partition" ASC;
-SELECT * FROM "report_columns" ORDER BY "column" ASC;
-SELECT * FROM "report_dtypes" ORDER BY "column" ASC;
